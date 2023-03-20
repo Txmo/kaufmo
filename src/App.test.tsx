13a837cd37d099ad900c515e14f9b77d6130f053
Timo Müller <t.imo_mueller@web.de>
2023-03-20 19:28:10 +0100

    Test if company and search field get rendered to the browser

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../src/App';
 
-test('renders learn react link', () => {
+test('renders at least one company', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/CmpName0/i);
+  expect(element).toBeInTheDocument();
 });
+
+test('renders the search field', () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText(/Search\.\.\./i);
+  expect(input).toBeInTheDocument();
+})
