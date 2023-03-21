8cd2874392833e44ca4ab56fdb26ea915517f895
Timo Müller <t.imo_mueller@web.de>
2023-03-21 10:12:49 +0100

    Remove console.log

--- src/CompanyList.tsx
+++ src/CompanyList.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Company from './Company';
 
-export default function CompanyList(){
-    
+export default function CompanyList() {
+
     const companyList = [];
-    for(let i = 0; i < 25; ++i){
+    for (let i = 0; i < 25; ++i) {
         companyList.push(<Company key={i} companyId={i} companyName={'CmpName' + i} />)
     }
-    
-    console.log(companyList);
+
     return (
         <div className="company-list-container">
             {companyList}
