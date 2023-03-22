874efb9c31c7940d51f3f72ce2c54a2c35e8bf87
Timo Müller <t.imo_mueller@web.de>
2023-03-22 21:11:14 +0100

    Remove unnecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import AppContainer from './AppContainer';
 import WikiBrands from './WikiBrands';
-import WikiParser from './WikiParser';
 
 class App extends React.Component {
   render(): React.ReactNode {
