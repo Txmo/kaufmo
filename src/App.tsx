dda83c4001dabc43bc85a4facf3e5641aeea35e1
Timo Müller <t.imo_mueller@web.de>
2023-03-22 20:56:41 +0100

    Move code that shall be executed after mount to function in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import AppContainer from './AppContainer';
 import WikiBrands from './WikiBrands';
+import WikiParser from './WikiParser';
 
-function App() {
-  return (
-    <div className="app">
-      <WikiBrands />
-      <AppContainer />
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount(): void {
+    const parser = new WikiParser();
+    const nodeList = document.querySelectorAll('.div-col > ul > li');
+    console.log(parser.parse(
+      nodeList as NodeListOf<HTMLLIElement>
+    ));
+  }
+  render(): React.ReactNode {
+    return (
+      <div className="app" >
+        <WikiBrands />
+        <AppContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
