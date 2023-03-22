2db4eb9435c990cdd1c920164c81ac08847663eb
Timo Müller <t.imo_mueller@web.de>
2023-03-22 21:13:58 +0100

    Remove wikibrands as they are only needed for creating json

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import AppContainer from './AppContainer';
-import WikiBrands from './WikiBrands';
 
 class App extends React.Component {
   render(): React.ReactNode {
     return (
       <div className="app" >
-        <WikiBrands />
         <AppContainer />
       </div>
     );
