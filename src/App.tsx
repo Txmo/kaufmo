128a5a6e24def9d02f3c4384ebf0b7fe0129f0db
Timo Müller <t.imo_mueller@web.de>
2023-03-23 12:27:42 +0100

    Add swiping navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import * as React from 'react'
 import AppContainer from './AppContainer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+function TestComponent() {
+  return (
+    <div>TestComponent</div>
+  )
+}
+
+const Tab = createMaterialTopTabNavigator();
 
 class App extends React.Component {
   render(): React.ReactNode {
     return (
       <div className="app" >
-        <AppContainer />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={AppContainer} />
+            <Tab.Screen name="Test" component={TestComponent} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </div>
     );
   }
