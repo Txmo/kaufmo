1de061656d826a9812852d53532598bb1113ba92
Timo Müller <t.imo_mueller@web.de>
2023-03-28 14:43:34 +0200

    Save and load shopping list items from localStorage

--- src/ShoppingListContainer.tsx
+++ src/ShoppingListContainer.tsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react"
+import { createCallChain } from "typescript";
 import ShoppingActionBar from "./ShoppingActionBar"
 import ShoppingList from "./ShoppingList"
 
 export default function ShoppingListContainer() {
+    const storageItem = localStorage.getItem('kaufmo_shopping_list');
+    let initList: Array<string>;
+    if (storageItem !== null) {
+        try {
+            initList = JSON.parse(storageItem);
+        } catch (e) {
+            initList = Array<string>();
+        }
+    } else {
+        initList = Array<string>();
+    }
     const [itemInput, setItemInput] = useState("");
-    const [itemList, setItemList] = useState(new Array<string>());
+    const [itemList, setItemList] = useState(initList);
 
     function addItem(name: string): void {
         if (name.length === 0) {
@@ -13,11 +25,13 @@ export default function ShoppingListContainer() {
         const newItemList = Array.from(itemList);
         newItemList.push(name);
         setItemList(newItemList)
+        localStorage.setItem('kaufmo_shopping_list', JSON.stringify(newItemList));
     }
 
     function removeItem(id: number): void {
         const newItemList = itemList.filter((v, index) => index !== id)
         setItemList(newItemList);
+        localStorage.setItem('kaufmo_shopping_list', JSON.stringify(newItemList));
     }
 
     return (
