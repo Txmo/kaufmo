09d4cdfc6c73b62d23ecb57cdf8b4154446fe4f4
Timo Müller <t.imo_mueller@web.de>
2023-03-28 14:44:01 +0200

    Remove unused import

--- src/ShoppingListContainer.tsx
+++ src/ShoppingListContainer.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-import { createCallChain } from "typescript";
 import ShoppingActionBar from "./ShoppingActionBar"
 import ShoppingList from "./ShoppingList"
 
